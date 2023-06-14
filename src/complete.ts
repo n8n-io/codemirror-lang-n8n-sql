eae7cf6353841dee06b9fef01b97d2db933942af
Milorad Filipovic <milorad@n8n.io>
2023-06-14 14:06:22 +0200

    Fix alias extraction logic and updated token tests to account for whitespaces in grammar tree

--- src/complete.ts
+++ src/complete.ts
@@ -4,6 +4,8 @@ import {syntaxTree} from "@codemirror/language"
 import {SyntaxNode} from "@lezer/common"
 import {Type, Keyword} from "./sql.grammar.terms"
 
+const SKIP_TOKENS = ['⚠'];
+
 function tokenBefore(tree: SyntaxNode) {
   let cursor = tree.cursor().moveTo(tree.from, -1)
   while (/Comment/.test(cursor.name)) cursor.moveTo(cursor.from, -1)
@@ -65,12 +67,19 @@ function getAliases(doc: Text, at: SyntaxNode) {
   }
   let aliases = null
   for (let scan = statement.firstChild, sawFrom = false, prevID: SyntaxNode | null = null; scan; scan = scan.nextSibling) {
+    if (SKIP_TOKENS.includes(scan.name)) continue;
     let kw = scan.name == "Keyword" ? doc.sliceString(scan.from, scan.to).toLowerCase() : null
     let alias = null
     if (!sawFrom) {
       sawFrom = kw == "from"
-    } else if (kw == "as" && prevID && plainID(scan.nextSibling)) {
-      alias = idName(doc, scan.nextSibling!)
+    } else if (kw == "as" && prevID) {
+      let next = scan.nextSibling;
+      if (SKIP_TOKENS.includes(next.name)) {
+        while (SKIP_TOKENS.includes(next.name)) {
+          next = next.nextSibling;
+        }
+      }
+      if (plainID(next)) alias = idName(doc, next)
     } else if (kw && EndFrom.has(kw)) {
       break
     } else if (prevID && plainID(scan)) {
